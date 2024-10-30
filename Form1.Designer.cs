21eb02aa73cb16ae99cbf9c911baae8de91bfc0a
KING-RPX <rodneymlotshwa@gmail.com>
2024-10-30 21:14:32 +0200

    first attempt at the form design

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,110 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblstudentid = new System.Windows.Forms.Label();
+            this.lblage = new System.Windows.Forms.Label();
+            this.lblname = new System.Windows.Forms.Label();
+            this.lblCourse = new System.Windows.Forms.Label();
+            this.txtstudentid = new System.Windows.Forms.TextBox();
+            this.txtcourse = new System.Windows.Forms.TextBox();
+            this.txtname = new System.Windows.Forms.TextBox();
+            this.txtage = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblstudentid
+            // 
+            this.lblstudentid.AutoSize = true;
+            this.lblstudentid.Location = new System.Drawing.Point(47, 61);
+            this.lblstudentid.Name = "lblstudentid";
+            this.lblstudentid.Size = new System.Drawing.Size(58, 13);
+            this.lblstudentid.TabIndex = 0;
+            this.lblstudentid.Text = "Student ID";
+            // 
+            // lblage
+            // 
+            this.lblage.AutoSize = true;
+            this.lblage.Location = new System.Drawing.Point(47, 137);
+            this.lblage.Name = "lblage";
+            this.lblage.Size = new System.Drawing.Size(26, 13);
+            this.lblage.TabIndex = 1;
+            this.lblage.Text = "Age";
+            // 
+            // lblname
+            // 
+            this.lblname.AutoSize = true;
+            this.lblname.Location = new System.Drawing.Point(47, 110);
+            this.lblname.Name = "lblname";
+            this.lblname.Size = new System.Drawing.Size(35, 13);
+            this.lblname.TabIndex = 2;
+            this.lblname.Text = "Name";
+            // 
+            // lblCourse
+            // 
+            this.lblCourse.AutoSize = true;
+            this.lblCourse.Location = new System.Drawing.Point(47, 85);
+            this.lblCourse.Name = "lblCourse";
+            this.lblCourse.Size = new System.Drawing.Size(40, 13);
+            this.lblCourse.TabIndex = 3;
+            this.lblCourse.Text = "Course";
+            // 
+            // txtstudentid
+            // 
+            this.txtstudentid.Location = new System.Drawing.Point(111, 54);
+            this.txtstudentid.Name = "txtstudentid";
+            this.txtstudentid.Size = new System.Drawing.Size(100, 20);
+            this.txtstudentid.TabIndex = 4;
+            // 
+            // txtcourse
+            // 
+            this.txtcourse.Location = new System.Drawing.Point(111, 78);
+            this.txtcourse.Name = "txtcourse";
+            this.txtcourse.Size = new System.Drawing.Size(100, 20);
+            this.txtcourse.TabIndex = 5;
+            // 
+            // txtname
+            // 
+            this.txtname.Location = new System.Drawing.Point(111, 103);
+            this.txtname.Name = "txtname";
+            this.txtname.Size = new System.Drawing.Size(100, 20);
+            this.txtname.TabIndex = 6;
+            // 
+            // txtage
+            // 
+            this.txtage.Location = new System.Drawing.Point(111, 130);
+            this.txtage.Name = "txtage";
+            this.txtage.Size = new System.Drawing.Size(100, 20);
+            this.txtage.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtage);
+            this.Controls.Add(this.txtname);
+            this.Controls.Add(this.txtcourse);
+            this.Controls.Add(this.txtstudentid);
+            this.Controls.Add(this.lblCourse);
+            this.Controls.Add(this.lblname);
+            this.Controls.Add(this.lblage);
+            this.Controls.Add(this.lblstudentid);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblstudentid;
+        private System.Windows.Forms.Label lblage;
+        private System.Windows.Forms.Label lblname;
+        private System.Windows.Forms.Label lblCourse;
+        private System.Windows.Forms.TextBox txtstudentid;
+        private System.Windows.Forms.TextBox txtcourse;
+        private System.Windows.Forms.TextBox txtname;
+        private System.Windows.Forms.TextBox txtage;
     }
 }
 
