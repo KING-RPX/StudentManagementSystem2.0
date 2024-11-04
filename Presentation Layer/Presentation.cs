fa76f67b4f86a16686c1c67ac438a56a83c34dc7
KING-RPX <rodneymlotshwa@gmail.com>
2024-11-04 17:56:01 +0200

    we created our different classes

---
+++ Presentation Layer/Presentation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManagementSystem2._0.Presentation_Layer
+{
+    internal class Presentation
+    {
+    }
+}
